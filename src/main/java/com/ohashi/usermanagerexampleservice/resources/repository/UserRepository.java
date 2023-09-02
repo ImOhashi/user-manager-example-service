6d85836a1afedf9db9e051715b7de4a9f09e9752
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-09-02 17:58:40 -0300

    fix: add repository annotation on user repository interface

--- src/main/java/com/ohashi/usermanagerexampleservice/resources/repository/UserRepository.java
+++ src/main/java/com/ohashi/usermanagerexampleservice/resources/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.ohashi.usermanagerexampleservice.resources.repository;
 import com.ohashi.usermanagerexampleservice.resources.model.UserDocument;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<UserDocument, ObjectId> {
 }
