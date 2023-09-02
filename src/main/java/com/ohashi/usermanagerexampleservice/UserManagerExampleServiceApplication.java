38aa6d297a24df37b0f8cf471a1e71415e5a5099
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-09-02 17:26:20 -0300

    first commit

---
+++ src/main/java/com/ohashi/usermanagerexampleservice/UserManagerExampleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ohashi.usermanagerexampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerExampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerExampleServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohashi/usermanagerexampleservice/UserManagerExampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.usermanagerexampleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerExampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
