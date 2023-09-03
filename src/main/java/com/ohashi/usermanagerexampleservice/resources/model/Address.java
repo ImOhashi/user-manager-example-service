729e88a69535ff69965f6e64f0fa269775e7031e
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-09-02 21:08:21 -0300

    fix: add address constructor

--- src/main/java/com/ohashi/usermanagerexampleservice/resources/model/Address.java
+++ src/main/java/com/ohashi/usermanagerexampleservice/resources/model/Address.java
@@ -1,8 +1,16 @@
 package com.ohashi.usermanagerexampleservice.resources.model;
 
 public class Address {
+
+    public Address(String zipCode, String street, String number, String state) {
+        this.zipCode = zipCode;
+        this.street = street;
+        this.number = number;
+        this.state = state;
+    }
+
     private String zipCode;
-    private String Street;
+    private String street;
     private String number;
     private String state;
 }
