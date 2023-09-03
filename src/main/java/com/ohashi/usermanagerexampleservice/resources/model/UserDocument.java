b121a0602b6543127cb5d3b643b0a1f1b6dee1ec
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-09-02 21:55:11 -0300

    fix: add email field to user document and inner class to build entity

--- src/main/java/com/ohashi/usermanagerexampleservice/resources/model/UserDocument.java
+++ src/main/java/com/ohashi/usermanagerexampleservice/resources/model/UserDocument.java
@@ -4,6 +4,7 @@ import com.ohashi.usermanagerexampleservice.resources.model.enums.UserType;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -12,13 +13,23 @@ import java.io.Serializable;
 public class UserDocument implements Serializable {
 
     @PersistenceCreator
-    public UserDocument(ObjectId _id, String name, String surname, String description, UserType userType, Address address) {
+    public UserDocument(ObjectId _id, String name, String surname, String email, String description, UserType userType, Address address) {
         this._id = _id;
         this.name = name;
         this.surname = surname;
+        this.email = email;
         this.description = description;
-        this.userType = userType;
+        this.type = userType;
         this.address = address;
+    }
+
+    private UserDocument(Builder builder) {
+        this.name = builder.name;
+        this.surname = builder.surname;
+        this.email = builder.email;
+        this.description = builder.description;
+        this.type = builder.type;
+        this.address = builder.address;
     }
 
     @Id
@@ -26,7 +37,54 @@ public class UserDocument implements Serializable {
 
     private String name;
     private String surname;
+
+    @Indexed
+    private String email;
     private String description;
-    private UserType userType;
+    private UserType type;
     private Address address;
+
+    public static class Builder {
+
+        private String name;
+        private String surname;
+        private String email;
+        private String description;
+        private UserType type;
+        private Address address;
+
+        public UserDocument build() {
+            return new UserDocument(this);
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setType(UserType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+    }
 }
