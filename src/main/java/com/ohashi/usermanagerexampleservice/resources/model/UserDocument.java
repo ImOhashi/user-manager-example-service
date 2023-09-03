08fda8131a17076dd745387d1563ca7483ea5a61
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-09-02 21:09:56 -0300

    fix: add persistence constructor to user document entity

--- src/main/java/com/ohashi/usermanagerexampleservice/resources/model/UserDocument.java
+++ src/main/java/com/ohashi/usermanagerexampleservice/resources/model/UserDocument.java
@@ -3,12 +3,23 @@ package com.ohashi.usermanagerexampleservice.resources.model;
 import com.ohashi.usermanagerexampleservice.resources.model.enums.UserType;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
 @Document("user")
 public class UserDocument implements Serializable {
+
+    @PersistenceCreator
+    public UserDocument(ObjectId _id, String name, String surname, String description, UserType userType, Address address) {
+        this._id = _id;
+        this.name = name;
+        this.surname = surname;
+        this.description = description;
+        this.userType = userType;
+        this.address = address;
+    }
 
     @Id
     private ObjectId _id;
